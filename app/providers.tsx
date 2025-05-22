00642c1b333e0d722620ba2959bb5621619aac0d
madgrv <mad.grv@gmail.com>
2025-05-22 19:14:06 +0100

    Added user profile context and edit profile page

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,14 +3,16 @@
 import * as React from 'react';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { AuthProvider } from '@/app/context/auth-context';
-
+import { ProfileProvider } from '@/app/context/profile-context';
 import { DietPlanProvider } from '@/app/context/DietPlanContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider attribute='class' defaultTheme='system' enableSystem>
       <AuthProvider>
-        <DietPlanProvider>{children}</DietPlanProvider>
+        <ProfileProvider>
+          <DietPlanProvider>{children}</DietPlanProvider>
+        </ProfileProvider>
       </AuthProvider>
     </NextThemesProvider>
   );
